39ee783e417bb3cf4285ff1361561af04d2ad438
Tagirov_zakir <zakir0556@gmail.com>
2021-04-08 19:04:17 +0500

    мобильное меню

---
+++ source/js/main-menu.js
@@ -0,0 +1,14 @@
+const toggle = document.querySelector(".page-header__toggle");
+const menu = document.querySelector(".site-menu");
+
+toggle.classList.remove("page-header__toggle--hide");
+menu.classList.add("site-menu--hide");
+
+menuToggle();
+
+function menuToggle() {
+  toggle.addEventListener("click", function () {
+    toggle.classList.toggle("page-header__toggle--close");
+    menu.classList.toggle("site-menu--hide");
+  })
+}
