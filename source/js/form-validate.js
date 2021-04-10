51bb4aab52180125b07f9d2ea119ad90ed484646
Tagirov_zakir <zakir0556@gmail.com>
2021-04-11 00:08:36 +0500

    поправил скрипт

--- source/js/form-validate.js
+++ source/js/form-validate.js
@@ -3,16 +3,22 @@ const inputRequired = document.querySelectorAll("input[required]");
 const form = document.querySelector(".questionary");
 
 button.addEventListener("click", function (evt) {
-  let sum = 0;
+  let errors = [];
   inputRequired.forEach(elem => {
     elem.classList.remove("questionary__input--error");
     if (!elem.validity.valid) {
+      errors.push(elem);
       elem.classList.add("questionary__input--error");
-      sum ++;
     }
   })
-  console.log(sum);
-  if (sum > 0) {
+  if (errors.length > 0) {
     evt.preventDefault();
+    scrollTo({left: 0, top: coords(errors[0]), behavior: "smooth"})
   }
 })
+
+function coords(elem) {
+  let error = elem.getBoundingClientRect();
+  error.y += pageYOffset;
+  return error.y - 10;
+}
