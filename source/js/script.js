34f2db78f25d3215d423f4fd91b8cf286eb2c4e7
Tagirov_zakir <zakir0556@gmail.com>
2021-04-26 00:46:40 +0500

    Автоматизация

--- source/js/script.js
+++ source/js/script.js
@@ -22,3 +22,18 @@ function coords(elem) {
   error.y += pageYOffset;
   return error.y - 10;
 }
+
+const toggle = document.querySelector(".page-header__toggle");
+const menu = document.querySelector(".site-menu");
+
+toggle.classList.remove("page-header__toggle--hide");
+menu.classList.add("site-menu--hide");
+
+menuToggle();
+
+function menuToggle() {
+  toggle.addEventListener("click", function () {
+    toggle.classList.toggle("page-header__toggle--close");
+    menu.classList.toggle("site-menu--hide");
+  })
+}
