bf20b0bc4c05165a9298917d47b99e8e19513a68
Tagirov_zakir <zakir0556@gmail.com>
2021-04-27 20:40:06 +0500

    слайдер

--- source/js/before-after.js
+++ source/js/before-after.js
@@ -1,21 +1,55 @@
-const before = document.querySelector(".buttons__button--before");
-const after = document.querySelector(".buttons__button--after");
+var mql = window.matchMedia('(max-width: 767px)');
+const buttons = document.querySelectorAll(".buttons__button");
 const slide = document.querySelector(".results__images");
 const bar = document.querySelector(".results__bar");
+const imageBox = document.querySelectorAll(".results__image-box");
 
-after.addEventListener("click", function (evt) {
-  evt.preventDefault();
-  before.removeAttribute("disabled");
-  after.setAttribute("disabled", "disabled");
+function mobileSlider(e) {
+  if (e.matches) {
+    buttons[0].setAttribute("disabled", "disabled");
+    buttons[1].removeAttribute("disabled");
+    imageBox.forEach(el => el.classList.remove("results__image-box--hide"));
+
+    buttons.forEach(button => {
+      button.removeEventListener("click", bigSlide)
+      button.addEventListener("click", minSlide)
+    })
+  } else {
+    buttons[0].setAttribute("disabled", "disabled");
+    buttons[1].removeAttribute("disabled");
+    slide.classList.remove("results__images--after");
+    bar.classList.remove("results__bar--after");
+    imageBox[1].classList.add("results__image-box--hide");
+
+    buttons.forEach(button => {
+      button.removeEventListener("click", minSlide)
+      button.addEventListener("click", bigSlide)
+    })
+  }
+}
+
+mql.addEventListener("change", mobileSlider);
+mobileSlider(mql);
+
+function minSlide() {
+  buttons.forEach(b => {
+    if (b.hasAttribute("disabled")) {
+      b.removeAttribute("disabled")
+    } else {
+      b.setAttribute("disabled", "disabled")
+    }
+  })
   slide.classList.toggle("results__images--after");
-  bar.classList.add("results__bar--after");
-})
+  bar.classList.toggle("results__bar--after");
+}
 
-before.addEventListener("click", function (evt) {
-  evt.preventDefault();
-  after.removeAttribute("disabled");
-  before.setAttribute("disabled", "disabled");
-  slide.classList.toggle("results__images--after");
-  bar.classList.remove("results__bar--after");
-})
-
+function bigSlide() {
+  buttons.forEach(b => {
+    if (b.hasAttribute("disabled")) {
+      b.removeAttribute("disabled")
+    } else {
+      b.setAttribute("disabled", "disabled")
+    }
+  })
+  imageBox.forEach(i => i.classList.toggle("results__image-box--hide"))
+}
